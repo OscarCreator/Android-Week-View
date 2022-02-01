278a84c56ea18228138b3b9fd2b81d8026c27a49
OscarCreator <oscar.creator13@gmail.com>
2022-02-01 10:48:25 +0100

    Added custom view to library.

---
+++ weekview/src/main/java/com/oscarcreator/weekview/WeekView.kt
@@ -0,0 +1,12 @@
+package com.oscarcreator.weekview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class WeekView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+}
